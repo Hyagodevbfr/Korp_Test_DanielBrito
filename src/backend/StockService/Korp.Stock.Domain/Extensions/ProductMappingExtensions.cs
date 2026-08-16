155e690cbf57754188998d30b6e954a505bb8f84
Daniel Brito <dev.danielbrito@gmail.com>
2026-08-15 22:06:37 -0300

    feat(stock-service): add product mapping extensions

---
+++ src/backend/StockService/Korp.Stock.Domain/Extensions/ProductMappingExtensions.cs
@@ -0,0 +1,26 @@
+using Korp.Stock.Domain.Common;
+using Korp.Stock.Domain.Dtos;
+using Korp.Stock.Domain.Entities;
+
+namespace Korp.Stock.Domain.Extensions;
+
+public static class ProductMappingExtensions
+{
+    public static ProductDto ToDto(this Product product)
+        => new(product.Id, product.Code, product.Description, product.Balance);
+
+    public static IEnumerable<ProductDto> ToDto(this IEnumerable<Product> products)
+        => products.Select(product => product.ToDto());
+
+    public static Result<Product> ToEntity(this CreateProductRequest request)
+        => Product.Create(request.Code, request.Description, request.Balance);
+
+    public static Result ApplyTo(this UpdateProductRequest request, Product product)
+        => product.Update(request.Code, request.Description);
+
+    public static Result ApplyTo(this AddStockRequest request, Product product)
+        => product.AddStock(request.Quantity);
+
+    public static Result ApplyTo(this RemoveStockRequest request, Product product)
+        => product.RemoveStock(request.Quantity);
+}
