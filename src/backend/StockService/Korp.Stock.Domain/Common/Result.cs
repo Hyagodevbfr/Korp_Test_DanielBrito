c66ce6e9fdfcce8856bbb5f795a47e79ba2f09b2
Daniel Brito <dev.danielbrito@gmail.com>
2026-08-15 22:05:43 -0300

    feat(stock-service): add generic result pattern

---
+++ src/backend/StockService/Korp.Stock.Domain/Common/Result.cs
@@ -0,0 +1,39 @@
+﻿namespace Korp.Stock.Domain.Common;
+
+public class Result
+{
+    protected Result(bool success, string? error = null)
+    {
+        IsSuccess = success;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public string? Error { get; }
+
+    public static Result Success()
+        => new(true);
+
+    public static Result Failure(string error)
+        => new(false, error);
+}
+
+public class Result<T> : Result
+{
+    private Result(T? value, bool success, string? error = null)
+        : base(success, error)
+    {
+        Value = value;
+    }
+
+    public T? Value { get; }
+
+    public static Result<T> Success(T value)
+        => new(value, true);
+
+    public static new Result<T> Failure(string error)
+        => new(default, false, error);
+}
