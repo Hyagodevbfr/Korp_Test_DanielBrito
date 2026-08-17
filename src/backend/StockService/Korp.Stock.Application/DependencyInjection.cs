5bce76e8a1bd21250247914d21cbe72e45c29fa3
Daniel Brito <dev.danielbrito@gmail.com>
2026-08-16 21:57:14 -0300

    feat(stock-service): register application services

---
+++ src/backend/StockService/Korp.Stock.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Korp.Stock.Application.Products.AddStock;
+using Korp.Stock.Application.Products.ConsumeStock;
+using Korp.Stock.Application.Products.CreateProduct;
+using Korp.Stock.Application.Products.GetProductById;
+using Korp.Stock.Application.Products.GetProducts;
+using Korp.Stock.Application.Products.UpdateProduct;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Korp.Stock.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<ICreateProductUseCase, CreateProductUseCase>();
+        services.AddScoped<IGetProductsUseCase, GetProductsUseCase>();
+        services.AddScoped<IGetProductByIdUseCase, GetProductByIdUseCase>();
+        services.AddScoped<IUpdateProductUseCase, UpdateProductUseCase>();
+        services.AddScoped<IAddStockUseCase, AddStockUseCase>();
+        services.AddScoped<IConsumeStockUseCase, ConsumeStockUseCase>();
+
+        return services;
+    }
+
+}
