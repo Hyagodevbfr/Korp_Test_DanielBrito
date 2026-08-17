ce17dfddfba87b6f90be3b7053dff6de33a3c54a
Daniel Brito <dev.danielbrito@gmail.com>
2026-08-16 21:54:25 -0300

    feat(stock-service): add product repository abstraction

---
+++ src/backend/StockService/Korp.Stock.Application/Abstractions/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Korp.Stock.Domain.Entities;
+
+namespace Korp.Stock.Application.Abstractions;
+
+public interface IProductRepository
+{
+    Task<Product?> GetByIdAsync(int id);
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<bool> ExistsByCodeAsync(string code, int? excludingId = null);
+    Task<IReadOnlyDictionary<int, Product>> GetByIdsAsync(IEnumerable<int> ids);
+    Task AddAsync(Product product);
+    Task SaveChangesAsync();
+}
